e478c42eea73b084fb1151c31a946ec5a5cee451
MarceDiaz <30642458+MarceDiaz@users.noreply.github.com>
2019-10-09 22:26:31 -0300

    By UI

---
+++ pages/searchPage.ts
@@ -0,0 +1,14 @@
+import { $, ElementFinder } from "protractor";
+
+export class SearchPageObject {
+    public searchTextBox: ElementFinder;
+    public searchButton: ElementFinder;
+    public logo: ElementFinder;
+
+    constructor() {
+        // this.searchTextBox = $("input[title='Search']");
+        this.searchTextBox = $("input[name='q']");
+        this.searchButton = $("input[value='Google Search']");
+        this.logo = $("div.logo img");
+    }
+}
